bd469c3a6e2326faa1ae9d765db589875fe38ad0
Carlos Ramos <carlosefrfloresta@gmail.com>
2019-08-18 10:56:13 -0300

    Implementation, test in list component

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -1,13 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { MatTableModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-describe('ListComponent', () => {
+fdescribe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        MatTableModule
+      ],
       declarations: [ ListComponent ]
     })
     .compileComponents();
@@ -19,7 +27,31 @@ describe('ListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display', () => {
+    const mockData = {name: 'Carlos', age: 30, email: 'email@gmail.com'};
+    component.data = [mockData];
+    fixture.detectChanges();
+    const queryHeader = Array.from(fixture.debugElement.nativeElement.querySelectorAll('th')).map( (e: any) => e.innerText.toLowerCase());
+    const queryRow = Array.from(fixture.debugElement.nativeElement.querySelectorAll('td')).map( (e: any) => e.innerText.toLowerCase());
+    const hasAllHeaders = Object.keys(mockData)
+      .reduce( (result: boolean, key: string) => result && queryHeader.includes(key.toLowerCase()) , true);
+    const hasAllRows = Object.values(mockData)
+      .map( v => `${v}`)
+      .reduce( (result: boolean, key: string) => result && queryRow.includes(key.toLowerCase()) , true);
+    expect(hasAllHeaders && hasAllRows).toBeTruthy();
+  });
+
+
+  it('should respond to user input', () => {
+    const mockData = {name: 'Carlos', age: 30, email: 'email@gmail.com'};
+    component.data = [mockData];
+    fixture.detectChanges();
+    const rowClickSpy = spyOn(component, 'onRowClick');
+    fixture.debugElement.nativeElement.querySelector('td').click();
+    expect(rowClickSpy).toHaveBeenCalled();
   });
 });
