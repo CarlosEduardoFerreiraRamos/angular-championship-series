dae1a18fa9f96ad87c580ae9eab66ebe39607d5c
Carlos Ramos <carlos.ramos@decoracontent.com>
2019-08-19 13:40:51 -0300

    FIX-01: testing in home page

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HomeModule } from './home.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatchService } from 'src/app/services/match-service/match.service';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from './home-routing.module';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,12 +14,19 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [
+        HttpClientTestingModule,
+        HomeModule,
+        RouterTestingModule.withRoutes(routes)
+      ],
+      providers: [MatchService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    const router = TestBed.get(Router);
+    router.initialNavigation();
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
